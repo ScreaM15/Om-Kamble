39a2de2e15ea1e2d1ddf4932a9ed978ebb3e6d21
ScreaM15 <omkamble308@gmail.com>
2022-08-08 20:08:39 +0530

    Android App

---
+++ app/src/main/java/com/example/appdesigns/FirstFragment.java
@@ -0,0 +1,48 @@
+package com.example.appdesigns;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import java.util.zip.Inflater;FragmentFirstBinding;
+
+public class FirstFragment<FragmentFirstBinding> extends Fragment {
+
+    private int binding;
+    private Inflater FragmentFirstBinding;
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = Integer.parseInt(null);
+    }
+
+}
